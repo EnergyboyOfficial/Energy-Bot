bdaca551d2b64ef289b87cdb16be7047f580b1d5
Energyboy <94549302+EnergyboyOfficial@users.noreply.github.com>
2023-08-11 20:29:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-#This code has been made by Energyboy / Timur, please do not copy or steal without my mention. Thank you!
 import discord
 import psutil
 import random
@@ -6,11 +5,10 @@ import random
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Replace 'YOUR_BOT_TOKEN' with the token you got from the Discord Developer Portal
 bot = discord.Client(intents=intents)
 
-# Function to get system stats
 def get_system_stats():
+    # Function to get system stats
     cpu_usage = psutil.cpu_percent()
     ram_usage = psutil.virtual_memory().percent
     disk_usage = psutil.disk_usage('/').percent
@@ -20,8 +18,8 @@ def get_system_stats():
 
     return f"CPU Usage: {cpu_usage}%\nRAM Usage: {ram_usage}%\nDisk Usage: {disk_usage}%\nNetwork Usage: {network_usage}"
 
-# Function to convert bytes to a more human-readable format
 def convert_bytes(bytes):
+    # Function to convert bytes to human-readable format
     sizes = ["B", "KB", "MB", "GB", "TB"]
     i = 0
     while bytes >= 1024 and i < len(sizes) - 1:
@@ -29,12 +27,10 @@ def convert_bytes(bytes):
         i += 1
     return f"{bytes:.2f} {sizes[i]}"
 
-# Event handler for when the bot is ready and connected to Discord
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-# Event handler for when a message is received
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
@@ -42,15 +38,11 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         await message.channel.send('Hello!')
-    
+
     if message.content.startswith('!stats'):
         system_stats = get_system_stats()
         await message.channel.send(system_stats)
-    
-    if message.content.startswith('!flip'):
-        coin_flip = 'Heads' if random.randint(0, 1) == 0 else 'Tails'
-        await message.channel.send(f"The coin landed on: {coin_flip}")
-    
+
     if message.content.startswith('!ping'):
         latency = bot.latency
         await message.channel.send(f"Pong! Latency: {latency*1000:.2f} ms")
@@ -64,38 +56,12 @@ async def on_message(message):
             random.seed(user_mentions[0].id + user_mentions[1].id)
             love_percentage = random.randint(0, 100)
             
-            await message.channel.send(f"💘 {user_mentions[0].mention} and {user_mentions[1].mention}'s love percentage is {love_percentage}%!")
+            await message.channel.send(f"?? {user_mentions[0].mention} and {user_mentions[1].mention}'s love percentage is {love_percentage}%!")
         except ValueError as e:
             await message.channel.send(str(e))
 
-    if message.content.startswith('!truth'):
-        truths = [
-            "What is your biggest fear?",
-            "What is your favorite food?",
-            "Have you ever had a crush on someone in this server?",
-            "What is the most embarrassing thing that has happened to you?",
-            "What is your hidden talent?",
-            "Have you ever told a secret you promised to keep?",
-            # Add more truth questions here
-        ]
-        truth = random.choice(truths)
-        await message.channel.send(f"🤫 {message.author.mention}, here's your truth question: {truth}")
-
-    if message.content.startswith('!dare'):
-        dares = [
-            "Sing your favorite song out loud.",
-            "Do your best impression of a famous celebrity.",
-            "Send a funny meme to a random member in this server.",
-            "Change your Discord nickname to 'Daredevil' for the next hour.",
-            "Call a friend and tell them a ridiculous story to see if they believe it.",
-            "Take a selfie and post it in the server's selfie channel.",
-            # Add more dare challenges here
-        ]
-        dare = random.choice(dares)
-        await message.channel.send(f"👀 {message.author.mention}, here's your dare challenge: {dare}")
-
     if message.content.startswith('!send_to_all'):
-        if message.author.id == 'YOUR_USER_ID':  # Replace 'YOUR_USER_ID' with your own Discord user ID
+        if message.author.id == YOURUSERID:  # Replace 'YOUR_USER_ID' with your own Discord user ID
             args = message.content.split(' ', 1)
             if len(args) < 2:
                 await message.channel.send("Please provide a message to send.")
@@ -106,5 +72,16 @@ async def on_message(message):
                 channel = guild.text_channels[0]  # Change this to the channel where you want to send the message
                 await channel.send(args[1])
 
-# Run the bot with your token
-bot.run('Bot-Token-Here')
+    if message.content.startswith('!help'):
+        help_message = """
+        **Available Commands:**
+        - !hello: Greet the bot
+        - !stats: Show system stats
+        - !ping: Check bot latency
+        - !lovecalc @user1 @user2: Calculate love percentage between two users
+        - !send_to_all <message>: Send a message to all servers the bot is in
+        - !help: Show this help message
+        """
+        await message.channel.send(help_message)
+
+bot.run('YOUR-BOT-TOKEN')
